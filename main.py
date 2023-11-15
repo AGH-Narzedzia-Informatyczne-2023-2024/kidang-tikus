6f086b0139dea1301090b3ce199a26ac3d78291e
jaanonim <mat8mro@gmail.com>
2023-11-15 12:13:41 +0100

    add main function to main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 deg = 123
 
-print("Program do sumowania")
+def main():
+    print("Program do sumowania")
 
-def suma(a, b):
-    return a + b
+    def suma(a, b):
+        return a + b
 
-print("Suma to %d" % suma(deg, 5))
+    print("Suma to %d" % suma(deg, 5))
+
+if __name__ == "__main__":
+    main()
