c128932950bdc7a011c6b86ca07ba53c9fd76ae5
adpawel <147630307+adpawel@users.noreply.github.com>
2023-11-16 21:57:07 +0100

    added function in average.py

--- average.py
+++ average.py
@@ -1,16 +1,24 @@
-ilosc_liczb = int(input("Podaj ilość liczb: "))
+def oblicz_srednia(ilosc_liczb):
+    # Inicjalizacja pustej tablicy na liczby
+    liczby = []
 
-# Inicjalizacja pustej tablicy na liczby
-liczby = []
-
-# Pętla for do wczytywania liczb
-for i in range(ilosc_liczb):
+    # Pętla for do wczytywania liczb
+    for i in range(ilosc_liczb):
     liczba = float(input(f"Podaj liczbę {i + 1}: "))
     liczby.append(liczba)
 
-# Obliczanie średniej
-if ilosc_liczb > 0:
-    srednia = round(sum(liczby) / ilosc_liczb, 3)
-    print(f"Średnia z podanych liczb wynosi: {srednia}")
-else:
-    print("Nie podano żadnych liczb, nie można obliczyć średniej.")
+    # Obliczanie średniej
+    if ilosc_liczb > 0:
+        srednia = round(sum(liczby) / ilosc_liczb, 3)
+        return srednia
+    else:
+        print("Nie podano żadnych liczb, nie można obliczyć średniej.")
+        return None
+    
+# Pobranie ilości liczb od użytkownika
+ilosc_liczb = int(input("Podaj ilość liczb: "))
+
+# Wywołanie funkcji i wyświetlenie wyniku
+wynik = oblicz_srednia(ilosc_liczb)
+if wynik is not None:
+    print(f"Średnia z podanych liczb wynosi: {wynik}")
